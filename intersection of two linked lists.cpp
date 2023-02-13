503f2a0b28c0a7d0252dd0b730b4af784cdae0e0
Khushi197 <113971416+Khushi197@users.noreply.github.com>
2023-02-13 12:12:08 +0530

    Create intersection of two linked lists.cpp

---
+++ intersection of two linked lists.cpp
@@ -0,0 +1,21 @@
+https://leetcode.com/problems/intersection-of-two-linked-lists/submissions/896994462/
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode*, int> m;
+        while(headA !=NULL){
+            m[headA]++;
+            headA = headA ->next;
+
+
+        }
+        while(headB != NULL){
+            if(m[headB] > 0){
+                return headB;
+            }
+            headB = headB -> next;
+        }
+        return NULL;
+        
+    }
+};
